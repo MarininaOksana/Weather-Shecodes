656f9d2c85d16e9525501aba12e7bc84f57ba8a0
MarininaOksana <113500677+MarininaOksana@users.noreply.github.com>
2022-12-25 17:02:25 +0200

    Add files via upload
    Додано файли

---
+++ index.js
@@ -0,0 +1,91 @@
+ let weather = [
+  paris: {
+    temp: 19.7,
+    humidity: 80,
+  }
+  tokyo: {
+    temp: 17.3,
+    humidity: 50,
+  }
+  lisbon: {
+    temp: 30.2,
+    humidity: 20,
+  }
+  "san francisco": {
+    temp: 20.9,
+    humidity: 100,
+  }
+  oslo: {
+    temp: -5,
+    humidity: 20,
+  }
+];
+
+let city = prompt("Enter a city?");
+city = city.toLowerCase();
+if (weather[city] !== undefined) {
+  let temperature = weather[city].temp;
+  let humidity = weather[city].humidity;
+  let celsiusTemperature = Math.round(temperature);
+  let fahrenheitTemperature = Math.round((temperature * 9) / 5 + 32);
+
+  alert(
+    `It is currently ${celsiusTemperature}°C (${fahrenheitTemperature}°F) in ${city} with a humidity of ${humidity}%`
+  );
+} else {
+  alert(
+    `Sorry we don't know the weather for this city, try going to https://www.google.com/search?q=weather+${city}`
+  );
+}
+
+function formatDate(date) {
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday"
+  ];
+let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December"
+  ];
+
+  let currentYear = date.getFullYear();
+  let currentDay = days[date.getDay()];
+  let currentMonth = months[date.getMonth()];
+  let currentDate = date.getDate();
+let formattedDate = `Today is ${currentDay}, ${currentMonth} ${currentDate}, ${currentYear}`;
+
+  return formattedDate;
+}
+
+console.log(formatDate(currentTime));
+  
+ function showWeather(response) {
+  let h1 = document.querySelector("h1");
+  let temperature = Math.round(response.data.main.temp);
+  h1.innerHTML = `It is currently ${temperature}° in ${response.data.name}`;
+}
+
+function retrievePosition(position) {
+  let apiKey = "5f472b7acba333cd8a035ea85a0d4d4c";
+  let lat = position.coords.latitude;
+  let lon = position.coords.longitude;
+  let url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=${apiKey}`;
+  axios.get(url).then(showWeather);
+}
+
+navigator.geolocation.getCurrentPosition(retrievePosition);
